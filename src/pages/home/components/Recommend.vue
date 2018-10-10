9608807aa17cf2bdc236817a1cb2c5b34aabc478
xlk <1148038404@qq.com>
2018-10-10 22:25:11 +0800

    add recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div class="title">猜你喜欢</div>
+      <ul class="border-bottom">
+        <li class="item" v-for="item of recommendList" :key="item.id">
+          <img :src="item.imgUrl" class="item-img">
+          <div class="item-info">
+            <p class="item-title">{{item.title}}</p>
+            <p class="item-desc">{{item.desc}}</p>
+            <button class="item-button">查看详情</button>
+          </div>
+        </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1809/87/8796d4dfe0366858a3.water.png_200x200_ca6e25bf.png',
+        title: '世界之窗',
+        desc: '带你看世界'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1809/87/8796d4dfe0366858a3.water.png_200x200_ca6e25bf.png',
+        title: '世界之窗',
+        desc: '带你看世界'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1809/87/8796d4dfe0366858a3.water.png_200x200_ca6e25bf.png',
+        title: '世界之窗',
+        desc: '带你看世界'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eeeeee
+    text-indent: .2rem
+  .item
+    overflow: hidden
+    display: flex
+    height: 1.9rem
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size : .32rem
+        ellipsis()
+      .item-desc
+        line-height : .4rem
+        color: #cccccc
+        ellipsis()
+      .item-button
+        line-height : .44rem
+        margin-top: .16rem
+        background : #ff9300
+        padding: 0 .2rem
+        border-radius: .06rem
+        color: #fff
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+      <ul class="border-bottom">
+        <li class="item" v-for="item of recommendList" :key="item.id">
+          <div class="item-img-wrapper">
+            <img :src="item.imgUrl" class="item-img">
+          </div>
+          <div class="item-info">
+            <p class="item-title">{{item.title}}</p>
+            <p class="item-desc">{{item.desc}}</p>
+          </div>
+        </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/fa/ca65fde9677de2.jpg_r_640x214_4500e3ff.jpg',
+        title: '世界之窗',
+        desc: '带你看世界'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/fa/ca65fde9677de2.jpg_r_640x214_4500e3ff.jpg',
+        title: '世界之窗',
+        desc: '带你看世界'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/fa/ca65fde9677de2.jpg_r_640x214_4500e3ff.jpg',
+        title: '世界之窗',
+        desc: '带你看世界'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eeeeee
+    text-indent: .2rem
+  .item-img-wrapper
+    height: 0
+    overflow: hidden
+    padding-bottom: 33.4%
+  .item-img
+    width: 100%
+  .item-info
+    padding: .1rem
+    .item-title
+      line-height: .54rem
+      font-size : .32rem
+      ellipsis()
+    .item-desc
+      line-height : .4rem
+      color: #cccccc
+      ellipsis()
+</style>
