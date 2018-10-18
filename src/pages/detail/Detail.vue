27f122d99399cc244a4f014201a6923a778aea0b
xlk <1148038404@qq.com>
2018-10-18 22:30:29 +0800

    detail list

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,18 +2,37 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import detailBanner from './components/Banner'
 import detailHeader from './components/Header'
+import detailList from './components/List'
 export default {
   name: 'Detail',
   components: {
     detailBanner,
-    detailHeader
+    detailHeader,
+    detailList
+  },
+  data () {
+    return {
+      list: [{
+        title: '成人票',
+        children: [{
+          title: '成人三馆联票'
+        }, {
+          title: '成人五馆联票'
+        }]
+      }, {
+        title: '学生票'}, {
+        title: '儿童票'}, {
+        title: '特惠票'}]
+    }
   }
 }
 </script>
